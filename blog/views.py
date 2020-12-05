3767806ed76642591d00fefa8cba429e87ea7cb5
shahid <shahid.ice.nstu@gmail.com>
2020-12-05 16:11:29 +0600

    post Add listview and detail view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# blog/views.py
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+            status='published',
+            publish__year=year,
+            publish__month=month,
+            publish__day=day)
+
+    return render(request,
+            'blog/post/detail.html',
+            {'post': post})
