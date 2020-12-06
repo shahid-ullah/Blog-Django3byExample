69836c87d10c33ced106120256f6d892b97ef1a3
shahid <shahid.ice.nstu@gmail.com>
2020-12-06 14:29:30 +0600

    Add canonical url for post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 # blog/models.py
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -33,6 +34,12 @@ class Post(models.Model):
                     default='draft')
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                args=[self.publish.year,
+                    self.publish.month,
+                    self.publish.day, self.slug])
 
     def __str__(self):
         return self.title
