07f29a0982c7e2062c4918684952ab1045c4d60d
shahid <shahid.ice.nstu@gmail.com>
2020-12-06 22:43:55 +0600

    change list view from function view to class view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 # blog/views.py
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 1
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
