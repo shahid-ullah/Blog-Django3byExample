b64c2c624a3229739e7d1806eecda4acc9db5f8d
shahid <shahid.ice.nstu@gmail.com>
2020-12-07 17:22:58 +0600

    Comment system added

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,13 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
-from .models import Post
+from .forms import CommentForm, EmailPostForm
+from .models import Comment, Post
 
 
 class PostListView(ListView):
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    # context_object_name = 'posts'
     paginate_by = 1
     template_name = 'blog/post/list.html'
 
@@ -44,9 +44,27 @@ def post_detail(request, year, month, day, post):
             publish__month=month,
             publish__day=day)
 
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
             'blog/post/detail.html',
-            {'post': post})
+            {'post': post,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form})
 
 
 def post_share(request, post_id):
