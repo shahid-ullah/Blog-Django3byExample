2ef6391ec8171a36f8b5e3359fff4e46497544a1
shahid <shahid.ice.nstu@gmail.com>
2020-12-08 23:20:38 +0600

    Add  similar post search

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # blog/views.py
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render, reverse
 from django.views.generic import ListView
 from taggit.models import Tag
@@ -69,12 +70,19 @@ def post_detail(request, year, month, day, post):
             #             post.publish.day, post.slug])
     else:
         comment_form = CommentForm()
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+            .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+            .order_by('-same_tags', '-publish')[:4]
 
     return render(request,
             'blog/post/detail.html',
             {'post': post,
             'comments': comments,
             'new_comment': new_comment,
+            'similar_posts': similar_posts,
             'comment_form': comment_form})
 
 
