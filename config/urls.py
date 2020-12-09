673b7680b29e2f5f0c70ff60487d70015a293394
shahid <shahid.ice.nstu@gmail.com>
2020-12-09 15:08:08 +0600

    add blog sitemap

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from blog.sitemaps import PostSitemap
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+
+sitemaps = {
+    'posts': PostSitemap,
+    }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
     path('__debug__/', include(debug_toolbar.urls)),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap')
 ]
